0ef67926087ed2a6878e8b1c32f387b135c1d3df
Reinier Zwitserloot <reinier@tipit.to>
2009-06-12 08:35:43 +0200

    For the strangest reason, compiling Getter with javac fails; it wants AccessLevel, but has AccessLevel. Yah, WTF. Turning AccessLevel into its FQN: lombok.AccessLevel fixes it. javac bug?

--- src/lombok/Getter.java
+++ src/lombok/Getter.java
@@ -8,5 +8,5 @@ import java.lang.annotation.Target;
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.CLASS)
 public @interface Getter {
-	AccessLevel value() default AccessLevel.PUBLIC;
+	AccessLevel value() default lombok.AccessLevel.PUBLIC;
 }
