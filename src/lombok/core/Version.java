d7e5537b9e5afd299bd88aa4566ca343ba12edd7
Reinier Zwitserloot <reinier@tipit.to>
2009-06-15 20:46:04 +0200

    Moved version.txt to lombok.core.Version.java.

---
+++ src/lombok/core/Version.java
@@ -0,0 +1,17 @@
+package lombok.core;
+
+public class Version {
+	private static final String VERSION = "0.2.0";
+	
+	private Version() {
+		//Prevent instantiation
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(VERSION);
+	}
+	
+	public static String getVersion() {
+		return VERSION;
+	}
+}
