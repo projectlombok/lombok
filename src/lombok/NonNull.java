388943a7e3c8d3cf64b44af045d4d3125b597444
Roel Spilker <r.spilker@gmail.com>
2009-08-01 02:52:16 +0200

    Introduced a NonNull annotation to generate null-checks

---
+++ src/lombok/NonNull.java
@@ -0,0 +1,12 @@
+package lombok;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE})
+@Retention(RetentionPolicy.CLASS)
+public @interface NonNull {
+
+}
