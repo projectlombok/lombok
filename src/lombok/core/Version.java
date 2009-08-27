c7a45b294d1d8464b529f2513bbca37ade7afb92
Reinier Zwitserloot <reinier@tipit.to>
2009-08-28 00:21:02 +0200

    WHOOPS - that version number should have been updated much earlier. My fault (reinierz).

--- src/lombok/core/Version.java
+++ src/lombok/core/Version.java
@@ -26,7 +26,7 @@ package lombok.core;
  */
 public class Version {
 	// ** CAREFUL ** - this class must always compile with 0 dependencies (it must not refer to any other sources or libraries).
-	private static final String VERSION = "0.8.3";
+	private static final String VERSION = "0.8.4-HEAD";
 	
 	private Version() {
 		//Prevent instantiation
