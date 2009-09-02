c46bf8576fb2424c107f970a90bc57bc6ed5204a
Reinier Zwitserloot <reinier@tipit.to>
2009-09-03 01:36:36 +0200

    "Fixed" issue #43" - problems with javax.validation.NotNull, which lombok used to copy over to the method parameter of generated setters and constructors, but NotNull is not valid there. We've fixed it by limiting ourselves to annotations named 'NonNull', popular versions of which are legal in all the places we copy them to.
    This leaves IDEA's NotNull out in the cold, but, lombok doesn't support IDEA anyway.
    
    This is a workaround - once lombok supports type introspection we'll fix this properly.

--- src/lombok/core/TransformationsUtil.java
+++ src/lombok/core/TransformationsUtil.java
@@ -83,7 +83,7 @@ public class TransformationsUtil {
 	public static final Pattern PRIMITIVE_TYPE_NAME_PATTERN = Pattern.compile(
 			"^(boolean|byte|short|int|long|float|double|char)$");
 	
-	public static final Pattern NON_NULL_PATTERN = Pattern.compile("^no[tn]null$", Pattern.CASE_INSENSITIVE);
+	public static final Pattern NON_NULL_PATTERN = Pattern.compile("^nonnull$", Pattern.CASE_INSENSITIVE);
 	public static final Pattern NULLABLE_PATTERN = Pattern.compile("^nullable$", Pattern.CASE_INSENSITIVE);
 	
 	/**
