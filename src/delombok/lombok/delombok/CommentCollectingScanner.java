9ac004b385f5fdc1a113a7b6fc38098c86e8ac8a
Reinier Zwitserloot <reinier@tipit.to>
2009-11-27 07:19:38 +0100

    Fixed whitespace to project standards.

--- src/delombok/lombok/delombok/CommentCollectingScanner.java
+++ src/delombok/lombok/delombok/CommentCollectingScanner.java
@@ -29,55 +29,52 @@ import com.sun.tools.javac.parser.Scanner;
 import com.sun.tools.javac.util.Context;
 
 public class CommentCollectingScanner extends Scanner {
-
-    private final Comments comments;
+	private final Comments comments;
 
 	/** A factory for creating scanners. */
-    public static class Factory extends Scanner.Factory {
-
-    	private final Context context;
-
+	public static class Factory extends Scanner.Factory {
+		private final Context context;
+		
 		public static void preRegister(final Context context) {
-		    context.put(scannerFactoryKey, new Context.Factory<Scanner.Factory>() {
+			context.put(scannerFactoryKey, new Context.Factory<Scanner.Factory>() {
 				public CommentCollectingScanner.Factory make() {
-				    return new Factory(context);
+					return new Factory(context);
 				}
-		    });
+			});
 		}
-	
+		
 		/** Create a new scanner factory. */
 		protected Factory(Context context) {
-		    super(context);
+			super(context);
 			this.context = context;
 		}
-
-        @Override
-        public Scanner newScanner(CharSequence input) {
-            if (input instanceof CharBuffer) {
-            	return new CommentCollectingScanner(this, (CharBuffer)input, context.get(Comments.class));
-            } 
-            char[] array = input.toString().toCharArray();
-            return newScanner(array, array.length);
-        }
+		
+		@Override
+		public Scanner newScanner(CharSequence input) {
+			if (input instanceof CharBuffer) {
+				return new CommentCollectingScanner(this, (CharBuffer)input, context.get(Comments.class));
+			}
+			char[] array = input.toString().toCharArray();
+			return newScanner(array, array.length);
+		}
+		
+		@Override
+		public Scanner newScanner(char[] input, int inputLength) {
+			return new CommentCollectingScanner(this, input, inputLength, context.get(Comments.class));
+		}
+	}
 	
-        @Override
-        public Scanner newScanner(char[] input, int inputLength) {
-            return new CommentCollectingScanner(this, input, inputLength, context.get(Comments.class));
-        }
-    }
-    
 	
 	public CommentCollectingScanner(CommentCollectingScanner.Factory factory, CharBuffer charBuffer, Comments comments) {
 		super(factory, charBuffer);
 		this.comments = comments;
 	}
-
-
+	
 	public CommentCollectingScanner(CommentCollectingScanner.Factory factory, char[] input, int inputLength, Comments comments) {
 		super(factory, input, inputLength);
 		this.comments = comments;
 	}
-
+	
 	@Override
 	protected void processComment(CommentStyle style) {
 		int prevEndPos = prevEndPos();
@@ -86,8 +83,7 @@ public class CommentCollectingScanner extends Scanner {
 		String content = new String(getRawCharacters(pos, endPos()));
 		comments.add(prevEndPos, pos, endPos(), content, newLine);
 	}
-
-
+	
 	private boolean containsNewLine(int from, int to) {
 		for (char c : getRawCharacters(from, to)) {
 			if (c == '\n' || c == '\r') {
