d5e7ae61c0d820be49a181a3f1755d84c0908058
Roel Spilker <r.spilker@gmail.com>
2009-12-07 16:05:19 +0100

    Added tests to see if we handle fields that start with a $ correctly

---
+++ test/delombok/resource/after/GetterWithDollar.java
@@ -0,0 +1,15 @@
+class GetterWithDollar1 {
+	int $i;
+	
+	public int getI() {
+		return $i;
+	}
+}
+class GetterWithDollar2 {
+	int $i;
+	int i;
+	
+	public int getI() {
+		return i;
+	}
+}
---
+++ test/delombok/resource/before/SetterWithDollar.java
@@ -0,0 +1,9 @@
+//ignore
+class SetterWithDollar1 {
+	@lombok.Setter int $i;
+}
+
+class SetterWithDollar2 {
+	@lombok.Setter int $i;
+	@lombok.Setter int i;
+}
