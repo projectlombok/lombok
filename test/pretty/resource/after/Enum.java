aa2643092d2f9e507c6647f69c7e31537f6b1c50
Reinier Zwitserloot <reinier@zwitserloot.com>
2010-01-07 22:49:51 +0100

    Javac's printer screws up on complex enums. Added tests for this case.

---
+++ test/pretty/resource/after/Enum.java
@@ -0,0 +1,35 @@
+enum Ranks {
+	CLUBS,
+	HEARTS,
+	DIAMONDS,
+	SPADES;
+}
+enum Complex {
+	RED("ff0000"),
+	GREEN("00ff00"),
+	BLUE("0000f");
+	private final String webColour;
+	Complex(String webColour) {
+		this.webColour = webColour;
+	}
+	public String getWebColour() {
+		return webColour;
+	}
+}
+enum Complexer {
+	RED {
+		public void foo() {
+		}
+		public void bar() {
+		}
+	},
+	GREEN("foo") {
+		public void foo() {
+		}
+	};
+	public abstract void foo();
+	Complexer(String colour) {
+	}
+	Complexer() {
+	}
+}
