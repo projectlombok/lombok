29e07a48755066f6a1bbaf6de12c3af3a9970d95
grootjans <grootjans@gmail.com>
2010-10-31 23:56:49 +0100

    Added after-delombok test files for Logger

---
+++ test/transform/resource/after-delombok/LoggerCommons.java
@@ -0,0 +1,6 @@
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+class LoggerCommons {
+	private static final Log log = LogFactory.getLog(LoggerCommons.class);
+}
---
+++ test/transform/resource/after-delombok/LoggerJul.java
@@ -0,0 +1,5 @@
+import java.util.logging.Logger;
+
+class LoggerJul {
+	private static final Logger log = Logger.getLogger("LoggerJul");	
+}
