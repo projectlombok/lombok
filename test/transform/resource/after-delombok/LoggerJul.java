5897d9685e4f606319a9707ae22efe4740b3f630
Roel Spilker <r.spilker@gmail.com>
2010-11-02 20:36:51 +0100

    Removed trailing tab

--- test/transform/resource/after-delombok/LoggerJul.java
+++ test/transform/resource/after-delombok/LoggerJul.java
@@ -1,5 +1,5 @@
 import java.util.logging.Logger;
 
 class LoggerJul {
-	private static final Logger log = Logger.getLogger("LoggerJul");	
+	private static final Logger log = Logger.getLogger("LoggerJul");
 }
