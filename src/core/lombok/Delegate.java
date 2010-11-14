339efa00c62267f8fb6643d8883d5130c9fdd05a
Roel Spilker <r.spilker@gmail.com>
2010-11-14 12:37:15 +0100

    Added meta-annotations

--- src/core/lombok/Delegate.java
+++ src/core/lombok/Delegate.java
@@ -21,6 +21,13 @@
  */
 package lombok;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.SOURCE)
 public @interface Delegate {
 	Class<?>[] value() default {};
 }
