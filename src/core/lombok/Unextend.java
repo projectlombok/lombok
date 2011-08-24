75856c9d08148ef91526e998a8428ee0e05e5678
Owner <Owner@Owner-HP>
2011-08-24 00:38:20 -0400

    Initial commit of unextend feature

---
+++ src/core/lombok/Unextend.java
@@ -0,0 +1,15 @@
+package lombok;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface Unextend {
+
+    Class<?>[] types() default {};
+
+    Class<?>[] excludes() default {};
+}
