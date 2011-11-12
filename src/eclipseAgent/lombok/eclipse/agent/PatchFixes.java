5cc928f471f3875f141ab1ee737cfe2613e9cdd6
jvanderhel <Jappe.vanderhel@gmail.com>
2011-11-12 19:13:28 +0100

    patched retrieveRightBraceOrSemiColonPosition && retrieveRightBrace so method bodies are converted instead of skipped Set Assignment.statementEnd to prevent invalid sourceRange in Statement convert

--- src/eclipseAgent/lombok/eclipse/agent/PatchFixes.java
+++ src/eclipseAgent/lombok/eclipse/agent/PatchFixes.java
@@ -52,6 +52,10 @@ public class PatchFixes {
 	public static int fixRetrieveEllipsisStartPosition(int original, int end) {
 		return original == -1 ? end : original;
 	}
+
+	public static int fixRetrieveRightBraceOrSemiColonPosition(int original, int end) {
+		return original == -1 ? end : original;
+	}
 	
 	public static final int ALREADY_PROCESSED_FLAG = 0x800000;	//Bit 24
 	
