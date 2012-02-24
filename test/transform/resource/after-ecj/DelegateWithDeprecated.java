c63d036e78790a6bd4750d778effe2a63914dbaa
Taiki Sugawara <buzz.taiki@gmail.com>
2012-02-24 22:56:42 +0900

    Remove spaces in after files.

--- test/transform/resource/after-ecj/DelegateWithDeprecated.java
+++ test/transform/resource/after-ecj/DelegateWithDeprecated.java
@@ -1,17 +1,12 @@
 class DelegateWithDeprecated {
-
 	private Bar bar;
-
 	private interface Bar {
-
 		@Deprecated
 		void deprecatedAnnotation();
 		/** @deprecated */
 		void deprecatedComment();
-
 		void notDeprecated();
 	}
-
 	@java.lang.Deprecated
 	@java.lang.SuppressWarnings("all")
 	public void deprecatedAnnotation() {
