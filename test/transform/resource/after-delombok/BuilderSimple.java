eb3d32c718d9ef46fd30bc677147cda85318fb9c
Reinier Zwitserloot <reinier@zwitserloot.com>
2013-06-18 03:33:35 +0200

    finished tests for builder (added after-delombok versions).

---
+++ test/transform/resource/after-delombok/BuilderSimple.java
@@ -0,0 +1,38 @@
+import java.util.List;
+class BuilderSimple<T> {
+	private final int noshow = 0;
+	private final int yes;
+	private List<T> also;
+	private int $butNotMe;
+	@java.lang.SuppressWarnings("all")
+	private BuilderSimple(final int yes, final List<T> also) {
+		this.yes = yes;
+		this.also = also;
+	}
+	@java.lang.SuppressWarnings("all")
+	public static class BuilderSimpleBuilder<T> {
+		private int yes;
+		private List<T> also;
+		@java.lang.SuppressWarnings("all")
+		BuilderSimpleBuilder() {
+		}
+		@java.lang.SuppressWarnings("all")
+		public BuilderSimpleBuilder<T> yes(final int yes) {
+			this.yes = yes;
+			return this;
+		}
+		@java.lang.SuppressWarnings("all")
+		public BuilderSimpleBuilder<T> also(final List<T> also) {
+			this.also = also;
+			return this;
+		}
+		@java.lang.SuppressWarnings("all")
+		public BuilderSimple<T> build() {
+			return new BuilderSimple<T>(yes, also);
+		}
+	}
+	@java.lang.SuppressWarnings("all")
+	public static <T> BuilderSimpleBuilder<T> builder() {
+		return new BuilderSimpleBuilder<T>();
+	}
+}
