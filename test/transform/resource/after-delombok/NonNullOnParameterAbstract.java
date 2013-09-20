72b55dccb18f38b8aefd0ac8e7c2e8bd2dd5c057
Sander Koning <askoning@gmail.com>
2013-09-20 17:13:39 +0200

    Issue 559: NullPointerException when @NonNull is used in abstract method - Prevent NPE in javac and give a proper warning in both eclipse and javac - Add test cases

---
+++ test/transform/resource/after-delombok/NonNullOnParameterAbstract.java
@@ -0,0 +1,10 @@
+abstract class NonNullOnParameterAbstract {
+	public void test(@lombok.NonNull String arg) {
+		if (arg == null) {
+			throw new java.lang.NullPointerException("arg");
+		}
+		System.out.println("Hey");
+	}
+	
+	public abstract void test2(@lombok.NonNull String arg);
+}
---
+++ test/transform/resource/before/NonNullOnParameterAbstract.java
@@ -0,0 +1,7 @@
+abstract class NonNullOnParameterAbstract {
+	public void test(@lombok.NonNull String arg) {
+		System.out.println("Hey");
+	}
+	
+	public abstract void test2(@lombok.NonNull String arg);
+}
