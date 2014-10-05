0d199eac39c9dcc32786e1ce9d98f2bb46820a70
Alexandru Bledea <admin@notbed.com>
2014-10-05 12:46:26 +0300

    remove author and add copyright

--- src/core/lombok/util/LombokGeneratorHelper.java
+++ src/core/lombok/util/LombokGeneratorHelper.java
@@ -1,5 +1,23 @@
-/**
+/*
+ * Copyright (C) 2014 The Project Lombok Authors.
  *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 package lombok.util;
 
@@ -11,7 +29,8 @@ import lombok.core.AST.Kind;
 import lombok.core.LombokNode;
 
 /**
- * @author Alexandru Bledea
+ * This class is intended to hold the implementation-independent code generation code
+ *
  * @since Oct 5, 2014
  */
 public final class LombokGeneratorHelper {
@@ -77,7 +96,6 @@ public final class LombokGeneratorHelper {
 	/**
 	 * Implementation specific class for code generators
 	 *
-	 * @author Alexandru Bledea
 	 * @since Oct 5, 2014
 	 */
 	public static interface ImplementationDetails {
