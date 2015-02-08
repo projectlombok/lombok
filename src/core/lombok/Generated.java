08710ee9bfc022923775c215e69433ff355665f7
Reinier Zwitserloot <reinier@zwitserloot.com>
2015-02-08 22:56:00 +0100

    Added lombok.Generated. We don’t do anything with this yet, but we will one day, and the more lombok.jars out in the wild that have this, the easier it’ll be.

---
+++ src/core/lombok/Generated.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2015 The Project Lombok Authors.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package lombok;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Lombok will eventually automatically add this annotation to all generated methods, fields, and classes.
+ * 
+ * You can mark the presence of this annotation as 'ignore it' for all code style and bug finding tools.
+ * <p>
+ * NB: As of v1.16.2 which introduces this annotation, lombok doesn't actually add this annotation; we're setting
+ * it up so that lombok jars in widespread use start having this, which will make it easier to actually apply it
+ * later on.
+ */
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.CLASS)
+public @interface Generated {
+}
