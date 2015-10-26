30cfbcf9e42f9f997817ad0d097b71655f9a8485
Roel Spilker <r.spilker@gmail.com>
2015-10-26 22:21:16 +0100

    Add tests for `@EqualsAndHashCode` for the attributes `of` and `exclude`

---
+++ test/transform/resource/after-delombok/EqualsAndHashcodeOfExclude.java
@@ -0,0 +1,46 @@
+final class EqualsAndHashCodeOf {
+	int x;
+	int y;
+	@java.lang.Override
+	@java.lang.SuppressWarnings("all")
+	@javax.annotation.Generated("lombok")
+	public boolean equals(final java.lang.Object o) {
+		if (o == this) return true;
+		if (!(o instanceof EqualsAndHashCodeOf)) return false;
+		final EqualsAndHashCodeOf other = (EqualsAndHashCodeOf)o;
+		if (this.x != other.x) return false;
+		return true;
+	}
+	@java.lang.Override
+	@java.lang.SuppressWarnings("all")
+	@javax.annotation.Generated("lombok")
+	public int hashCode() {
+		final int PRIME = 59;
+		int result = 1;
+		result = result * PRIME + this.x;
+		return result;
+	}
+}
+final class EqualsAndHashCodeExclude {
+	int x;
+	int y;
+	@java.lang.Override
+	@java.lang.SuppressWarnings("all")
+	@javax.annotation.Generated("lombok")
+	public boolean equals(final java.lang.Object o) {
+		if (o == this) return true;
+		if (!(o instanceof EqualsAndHashCodeExclude)) return false;
+		final EqualsAndHashCodeExclude other = (EqualsAndHashCodeExclude)o;
+		if (this.x != other.x) return false;
+		return true;
+	}
+	@java.lang.Override
+	@java.lang.SuppressWarnings("all")
+	@javax.annotation.Generated("lombok")
+	public int hashCode() {
+		final int PRIME = 59;
+		int result = 1;
+		result = result * PRIME + this.x;
+		return result;
+	}
+}
