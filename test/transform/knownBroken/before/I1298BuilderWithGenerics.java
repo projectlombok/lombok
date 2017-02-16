8ec7abf996ba1089104f5d56cc417b17dcadca5f
Reinier Zwitserloot <reinier@zwitserloot.com>
2017-02-16 22:16:34 +0100

    Reproduces the bug with recursive generics in builder (issue #1298).

---
+++ test/transform/knownBroken/before/I1298BuilderWithGenerics.java
@@ -0,0 +1,15 @@
+import java.util.Set;
+import lombok.Builder;
+import lombok.Value;
+
+public class I1298BuilderWithGenerics {
+  interface Inter<T, U extends Inter<T, U>> {}
+
+  @Builder @Value public static class Test<
+    Foo,
+    Bar extends Set<Foo>,
+    Quz extends Inter<Bar, Quz>> {
+      Foo foo;
+      Bar bar;
+    }
+}
