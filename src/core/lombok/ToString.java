98b0d91d091b554cb3ab9ed9f972d358c7cae01a
Patrick Cornelißen <cornelis@pcornelissen.de>
2017-11-04 11:45:41 +0100

    fix JavaDoc of callSuper in the ToString annotation

--- src/core/lombok/ToString.java
+++ src/core/lombok/ToString.java
@@ -64,7 +64,7 @@ public @interface ToString {
 	 * Include the result of the superclass's implementation of {@code toString} in the output.
 	 * <strong>default: false</strong>
 	 * 
-	 * @return Whether to call the superclass's {@code equals} implementation as part of the generated equals algorithm.
+	 * @return Whether to call the superclass's {@code toString} implementation as part of the generated equals algorithm.
 	 */
 	boolean callSuper() default false;
 	
