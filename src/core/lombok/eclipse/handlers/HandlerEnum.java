613a60dde360a2eca8e7d2eba8211e6a3af52827
Jan Matèrne <jan@materne.de>
2017-12-22 10:53:50 +0100

    use SetGeneratedByVisitor instead of explicit call for each artefact

--- src/core/lombok/eclipse/handlers/HandlerEnum.java
+++ src/core/lombok/eclipse/handlers/HandlerEnum.java
@@ -2,9 +2,7 @@ package lombok.eclipse.handlers;
 
 import static lombok.eclipse.handlers.EclipseHandlerUtil.*;
 
-import java.io.BufferedWriter;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.lang.reflect.Modifier;
 import java.util.Collections;
@@ -15,42 +13,32 @@ import org.eclipse.jdt.internal.compiler.ast.AbstractMethodDeclaration;
 import org.eclipse.jdt.internal.compiler.ast.Annotation;
 import org.eclipse.jdt.internal.compiler.ast.Argument;
 import org.eclipse.jdt.internal.compiler.ast.Block;
-import org.eclipse.jdt.internal.compiler.ast.ClassLiteralAccess;
 import org.eclipse.jdt.internal.compiler.ast.EqualExpression;
 import org.eclipse.jdt.internal.compiler.ast.Expression;
 import org.eclipse.jdt.internal.compiler.ast.FieldDeclaration;
-import org.eclipse.jdt.internal.compiler.ast.FieldReference;
 import org.eclipse.jdt.internal.compiler.ast.ForeachStatement;
 import org.eclipse.jdt.internal.compiler.ast.IfStatement;
 import org.eclipse.jdt.internal.compiler.ast.LocalDeclaration;
 import org.eclipse.jdt.internal.compiler.ast.MessageSend;
 import org.eclipse.jdt.internal.compiler.ast.MethodDeclaration;
-import org.eclipse.jdt.internal.compiler.ast.NameReference;
 import org.eclipse.jdt.internal.compiler.ast.NullLiteral;
 import org.eclipse.jdt.internal.compiler.ast.QualifiedNameReference;
-import org.eclipse.jdt.internal.compiler.ast.Receiver;
-import org.eclipse.jdt.internal.compiler.ast.Reference;
 import org.eclipse.jdt.internal.compiler.ast.ReturnStatement;
 import org.eclipse.jdt.internal.compiler.ast.SingleNameReference;
 import org.eclipse.jdt.internal.compiler.ast.Statement;
-import org.eclipse.jdt.internal.compiler.ast.ThisReference;
 import org.eclipse.jdt.internal.compiler.ast.TypeDeclaration;
 import org.eclipse.jdt.internal.compiler.ast.TypeReference;
 import org.eclipse.jdt.internal.compiler.classfmt.ClassFileConstants;
-import org.eclipse.jdt.internal.compiler.lookup.BlockScope;
-import org.eclipse.jdt.internal.compiler.lookup.ClassScope;
 import org.mangosdk.spi.ProviderFor;
-
-import com.sun.tools.javac.util.Name;
 
 import lombok.AccessLevel;
 import lombok.Enum;
 import lombok.core.AST.Kind;
 import lombok.core.AnnotationValues;
-import lombok.core.handlers.HandlerUtil;
 import lombok.eclipse.Eclipse;
 import lombok.eclipse.EclipseAnnotationHandler;
 import lombok.eclipse.EclipseNode;
+import lombok.eclipse.handlers.EclipseHandlerUtil.MemberExistsResult;
 import lombok.eclipse.handlers.HandleConstructor.SkipIfConstructorExists;
 
 @ProviderFor(EclipseAnnotationHandler.class) public class HandlerEnum extends EclipseAnnotationHandler<lombok.Enum> {
@@ -108,7 +96,7 @@ import lombok.eclipse.handlers.HandleConstructor.SkipIfConstructorExists;
 			AbstractMethodDeclaration createdFinder = finderGenerator.createFinder();
 
 			SetGeneratedByVisitor setGeneratedByVisitor = new SetGeneratedByVisitor(classNode.get());
-			//createdFinder.traverse(setGeneratedByVisitor, createdFinder.scope);
+			createdFinder.traverse(setGeneratedByVisitor, createdFinder.scope);
 			
 			injectMethod(classNode, createdFinder);
 		}
@@ -185,11 +173,11 @@ import lombok.eclipse.handlers.HandleConstructor.SkipIfConstructorExists;
 		}
 		
 		private Argument[] arguments() {
-			int pS = source.sourceStart, pE = source.sourceEnd;
-			long p = (long) pS << 32 | pE;
-			Argument argument = new Argument(fieldNode.getName().toCharArray(), p, copyType(fieldDeclaration.type, source), Modifier.FINAL);
-			argument.sourceStart = pS;
-			argument.sourceEnd = pE;
+			char[] argumentName = fieldNode.getName().toCharArray();
+			long posNom = (long) source.sourceStart << 32 | source.sourceEnd;
+			TypeReference argumentType = copyType(fieldDeclaration.type, source);
+			int argumentModifier = Modifier.FINAL;
+			Argument argument = new Argument(argumentName, posNom, argumentType, argumentModifier);
 			return new Argument[] {argument};
 		}
 		
@@ -203,69 +191,43 @@ import lombok.eclipse.handlers.HandleConstructor.SkipIfConstructorExists;
 		// }
 		private Statement[] body() {
 			return new Statement[] {
-//				x(),
 				foreachLoop(), 
 				finallyReturnNull()
 			};
 		}
 		
-		private Statement x() {
-			char[] nameOfIterationVariable = "value".toCharArray();
-			LocalDeclaration iterationVariable = new LocalDeclaration(nameOfIterationVariable, source.sourceStart, source.sourceEnd);;
-			iterationVariable.type = classAsType;
-			iterationVariable.initialization = new NullLiteral(source.sourceStart, source.sourceEnd);
-			fixASTNode(iterationVariable);
-			return iterationVariable;
-		}
-
 		private Statement foreachLoop() {
 			char[] nameOfIterationVariable = "value".toCharArray();
 
 			// for( >>>Type value<<< : values() ) { ... }
 			LocalDeclaration iterationVariable = new LocalDeclaration(nameOfIterationVariable, source.sourceStart, source.sourceEnd);;
 			iterationVariable.type = classAsType;
-			fixASTNode(iterationVariable);
 
+			// for(Type value : >>>values()<<< ) { ... }
+			MessageSend valuesMethod = new MessageSend();
+			valuesMethod.selector = "values".toCharArray();
+			valuesMethod.receiver = classAsQualifiedNameReference();
+			
 			// if (??condition??) return value;
 			Expression condition = createFieldComparison(iterationVariable);
 			Expression returnValue = new SingleNameReference(nameOfIterationVariable, source.sourceStart);
 			Statement thenStatement = new ReturnStatement(returnValue, source.sourceStart, source.sourceEnd);;
 			Statement comparisonStatement = new IfStatement(condition, asBlock(thenStatement), source.sourceStart, source.sourceEnd);
-			fixASTNode(condition);
-			fixASTNode(returnValue);
-			fixASTNode(thenStatement);
-			fixASTNode(comparisonStatement);
-			
-			// for(Type value : >>>values()<<< ) { ... }
-			MessageSend valuesMethod = new MessageSend();
-			valuesMethod.selector = "values".toCharArray();
-			valuesMethod.receiver = classAsQualifiedNameReference();
-			fixASTNode(valuesMethod);
 			
 			// for( ... ) { ... }
 			ForeachStatement foreachStatement = new ForeachStatement(iterationVariable, source.sourceStart);
 			foreachStatement.collection = valuesMethod;
 			foreachStatement.action = asBlock(comparisonStatement);
-			fixASTNode(foreachStatement);
 			
 			return foreachStatement;
 		}
 		
 		private QualifiedNameReference classAsQualifiedNameReference() {
-			
-			out.printf("classDecl.name='%s'%n", String.valueOf(classDeclaration.name));
-			
-			char[][] tokens = {"Farbe".toCharArray()};
+			char[][] tokens = {classDeclaration.name};
 			long[] pos = {0L};
 			return new QualifiedNameReference(
 				tokens, pos	, source.sourceStart, source.sourceEnd
 			);
-		}
-		
-		private void fixASTNode(ASTNode node) {
-			node.sourceStart = source.sourceStart;
-			node.sourceEnd = source.sourceEnd;
-			setGeneratedBy(node, source);
 		}
 		
 		private Block asBlock(Statement... statements) {
@@ -289,7 +251,6 @@ import lombok.eclipse.handlers.HandleConstructor.SkipIfConstructorExists;
 			Expression left = HandleToString.generateQualifiedNameRef(source, "value".toCharArray(), fieldNode.getName().toCharArray());
 			Expression right = new SingleNameReference(fieldNode.getName().toCharArray(), source.sourceStart);
 			EqualExpression equalExpression = new EqualExpression(left, right, EqualExpression.EQUAL_EQUAL);
-			fixASTNode(equalExpression);
 			return equalExpression;
 		}
 		
@@ -299,12 +260,9 @@ import lombok.eclipse.handlers.HandleConstructor.SkipIfConstructorExists;
 			Expression parameter = new SingleNameReference(fieldNode.getName().toCharArray(), source.sourceStart);
 			
 			MessageSend equals = new MessageSend();
-			equals.sourceStart = source.sourceStart;
-			equals.sourceEnd = source.sourceEnd;
 			equals.receiver = foreachVariable;
 			equals.selector = "equals".toCharArray();
 			equals.arguments = new Expression[] {parameter};
-			fixASTNode(equals);
 			
 			return equals;
 		}
@@ -313,7 +271,6 @@ import lombok.eclipse.handlers.HandleConstructor.SkipIfConstructorExists;
 		private Statement finallyReturnNull() {
 			NullLiteral nullLiteral = new NullLiteral(source.sourceStart, source.sourceEnd);
 			ReturnStatement returnStatement = new ReturnStatement(nullLiteral, source.sourceStart, source.sourceEnd);
-			fixASTNode(returnStatement);
 			return returnStatement;
 		}
 	}
