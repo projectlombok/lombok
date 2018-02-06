fee0ad33abceb0219076dde05c9d5d9ba950000f
Reinier Zwitserloot <reinier@zwitserloot.com>
2018-02-06 05:42:03 +0100

    [jdk9] add support for using lombok with JDK9 code when compiling using the new module syntax, and having module-info.java files in your source.

---
+++ src/core9/module-info.java
@@ -0,0 +1,18 @@
+module lombok {
+	requires java.compiler;
+	requires java.instrument;
+	requires jdk.unsupported;
+	
+	exports lombok;
+	exports lombok.experimental;
+	exports lombok.extern.apachecommons;
+	exports lombok.extern.java;
+	exports lombok.extern.jbosslog;
+	exports lombok.extern.log4j;
+	exports lombok.extern.slf4j;
+	
+	provides javax.annotation.processing.Processor with lombok.launch.AnnotationProcessorHider.AnnotationProcessor;
+//	provides javax.annotation.processing.Processor with lombok.launch.AnnotationProcessorHider.ClaimingProcessor;
+	provides org.mapstruct.ap.spi.AstModifyingAnnotationProcessor with lombok.launch.AnnotationProcessorHider.AstModificationNotifier;
+}
+
