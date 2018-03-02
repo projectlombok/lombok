3b46a241604496d716a57f078e27a254b03757c4
Krzysztof Krason <Krzysztof.Krason@gmail.com>
2018-03-02 16:43:25 +0100

    updated java 10 download

--- docker/provision/jdk/java-10.sh
+++ docker/provision/jdk/java-10.sh
@@ -1,4 +1,4 @@
 apt-get update && apt-get install -y wget
-wget https://download.java.net/java/jdk10/archive/42/BCL/jdk-10-ea+42_linux-x64_bin.tar.gz -O jdk.tar.gz
+wget https://download.java.net/java/jdk10/archive/45/BCL/jdk-10+45_linux-x64_bin.tar.gz -O jdk.tar.gz
 tar -xzf jdk.tar.gz -C /opt
 mv /opt/jdk-10 /opt/jdk
