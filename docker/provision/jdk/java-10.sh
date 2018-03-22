6fa43957d70a2de2bb31e6a0e4f6978bf4a78d47
Krzysztof Krason <Krzysztof.Krason@gmail.com>
2018-03-22 09:51:06 +0100

    using GA java 10

--- docker/provision/jdk/java-10.sh
+++ docker/provision/jdk/java-10.sh
@@ -1,4 +1,4 @@
 apt-get update && apt-get install -y wget
-wget https://download.java.net/java/jdk10/archive/45/BCL/jdk-10+45_linux-x64_bin.tar.gz -O jdk.tar.gz
+wget --inet4-only -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/10+46/76eac37278c24557a3c4199677f19b62/jdk-10_linux-x64_bin.tar.gz -O jdk.tar.gz
 tar -xzf jdk.tar.gz -C /opt
 mv /opt/jdk-10 /opt/jdk
