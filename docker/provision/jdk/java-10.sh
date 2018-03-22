16b77c03bf19378e103fbf58ca30e270005d9488
Krzysztof Krason <Krzysztof.Krason@gmail.com>
2018-03-22 09:52:09 +0100

    removed inet4 enforcement

--- docker/provision/jdk/java-10.sh
+++ docker/provision/jdk/java-10.sh
@@ -1,4 +1,4 @@
 apt-get update && apt-get install -y wget
-wget --inet4-only -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/10+46/76eac37278c24557a3c4199677f19b62/jdk-10_linux-x64_bin.tar.gz -O jdk.tar.gz
+wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/10+46/76eac37278c24557a3c4199677f19b62/jdk-10_linux-x64_bin.tar.gz -O jdk.tar.gz
 tar -xzf jdk.tar.gz -C /opt
 mv /opt/jdk-10 /opt/jdk
