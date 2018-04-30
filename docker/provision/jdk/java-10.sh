3ea12bdeba0936b4022e731282fd33c9c077e2d7
Roel Spilker <r.spilker@gmail.com>
2018-04-30 22:14:35 +0200

    jdk10: update download for the jdk

--- docker/provision/jdk/java-10.sh
+++ docker/provision/jdk/java-10.sh
@@ -1,4 +1,4 @@
 apt-get update && apt-get install -y wget
-wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/10+46/76eac37278c24557a3c4199677f19b62/jdk-10_linux-x64_bin.tar.gz -O jdk.tar.gz
+wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/10.0.1+10/fb4372174a714e6b8c52526dc134031e/jdk-10.0.1_linux-x64_bin.tar.gz -O jdk.tar.gz
 tar -xzf jdk.tar.gz -C /opt/
-mv /opt/jdk-10 /opt/jdk
+mv /opt/jdk-10.0.1 /opt/jdk
