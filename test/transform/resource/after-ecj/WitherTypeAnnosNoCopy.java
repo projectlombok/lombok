47ea7b182e90d2c7a26e548e6defd93b305d5ada
Werner Dietl <wdietl@gmail.com>
2018-08-30 23:18:33 -0400

    Eclipse expected output.

---
+++ test/transform/resource/after-ecj/WitherTypeAnnosNoCopy.java
@@ -0,0 +1,16 @@
+import lombok.experimental.Wither;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+import java.util.List;
+@Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER}) @interface TA {
+}
+class WitherTypeAnnos {
+  final @Wither @TA List<@TA String> foo;
+  WitherTypeAnnos(@TA List<@TA String> foo) {
+    super();
+    this.foo = foo;
+  }
+  public @java.lang.SuppressWarnings("all") WitherTypeAnnos withFoo(final List<String> foo) {
+    return ((this.foo == foo) ? this : new WitherTypeAnnos(foo));
+  }
+}
