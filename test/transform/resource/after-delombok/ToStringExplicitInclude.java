89294fe03c4257e13971dd9db3738511dc01e433
Roel Spilker <r.spilker@gmail.com>
2018-10-15 23:19:55 +0200

    add tests to ensure onlyExplicitlyIncluded means nothing if no fields are annotated. Fixes #1772

---
+++ test/transform/resource/after-delombok/ToStringExplicitInclude.java
@@ -0,0 +1,8 @@
+class ToStringExplicitInclude {
+	int x;
+	@java.lang.Override
+	@java.lang.SuppressWarnings("all")
+	public java.lang.String toString() {
+		return "ToStringExplicitInclude()";
+	}
+}
---
+++ test/transform/resource/after-ecj/ToStringExplicitInclude.java
@@ -0,0 +1,9 @@
+@lombok.ToString(onlyExplicitlyIncluded = true) class ToStringExplicitInclude {
+  int x;
+  ToStringExplicitInclude() {
+    super();
+  }
+  public @java.lang.Override @java.lang.SuppressWarnings("all") java.lang.String toString() {
+    return "ToStringExplicitInclude()";
+  }
+}
