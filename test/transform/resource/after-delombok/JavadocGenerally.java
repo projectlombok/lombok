5acc82e34b3ce658761b53e4dc230fcedb4b7b98
Emil Lundberg <emil@yubico.com>
2019-01-03 18:36:12 +0100

    Revert "Expand JavadocGenerally test to cover current getter/setter copying behaviour"
    This reverts commit e41f14ca1ad9c0b13d8f4d1c8646d23c8435960f.

--- test/transform/resource/after-delombok/JavadocGenerally.java
+++ test/transform/resource/after-delombok/JavadocGenerally.java
@@ -15,31 +15,6 @@ class JavadocGenerally {
 	 */
 	private int someField;
 	/**
-	 * Doc on field
-	 * <pre>
-	 * 	// code
-	 * </pre>
-	 */
-	private int someBeanField;
-	/**
-	 * This will remain on the field
-	 * @param foo is not removed when there's a GETTER section
-	 * @return is not removed when there's a GETTER section
-	 */
-	private int someGetterField;
-	/**
-	 * This will remain on the field
-	 * @param foo is not removed when there's a SETTER section
-	 * @return is not removed when there's a SETTER section
-	 */
-	private int someSetterField;
-	/**
-	 * This will remain on the field
-	 * @param foo is not removed
-	 * @return is not removed
-	 */
-	private int someGetterSetterField;
-	/**
 	 * Doc on method
 	 */
 	public void test() {
@@ -48,64 +23,5 @@ class JavadocGenerally {
 	 * Doc on inner
 	 */
 	public interface TestingInner {
-	}
-	/**
-	 * Doc on field
-	 * <pre>
-	 * 	// code
-	 * </pre>
-	 *
-	 * @return the value of the field
-	 */
-	@java.lang.SuppressWarnings("all")
-	public int getSomeBeanField() {
-		return this.someBeanField;
-	}
-	/**
-	 * Doc on field
-	 * <pre>
-	 * 	// code
-	 * </pre>
-	 * @param someBeanField the new value for the field
-	 */
-	@java.lang.SuppressWarnings("all")
-	public void setSomeBeanField(final int someBeanField) {
-		this.someBeanField = someBeanField;
-	}
-	/**
-	 * This will be moved to the getter
-	 * @param foo is not removed from a GETTER section
-	 * @return is not removed from a GETTER section
-	 */
-	@java.lang.SuppressWarnings("all")
-	public int getSomeGetterField() {
-		return this.someGetterField;
-	}
-	/**
-	 * This will be moved to the setter
-	 * @param foo is not removed from a SETTER section
-	 * @return is not removed from a SETTER section
-	 */
-	@java.lang.SuppressWarnings("all")
-	public void setSomeSetterField(final int someSetterField) {
-		this.someSetterField = someSetterField;
-	}
-	/**
-	 * This will be moved to the getter
-	 * @param foo is not removed from a GETTER section
-	 * @return is not removed from a GETTER section
-	 */
-	@java.lang.SuppressWarnings("all")
-	public int getSomeGetterSetterField() {
-		return this.someGetterSetterField;
-	}
-	/**
-	 * This will be moved to the setter
-	 * @param foo is not removed from a SETTER section
-	 * @return is not removed from a SETTER section
-	 */
-	@java.lang.SuppressWarnings("all")
-	public void setSomeGetterSetterField(final int someGetterSetterField) {
-		this.someGetterSetterField = someGetterSetterField;
 	}
 }
