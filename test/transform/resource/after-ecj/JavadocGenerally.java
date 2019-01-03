e41f14ca1ad9c0b13d8f4d1c8646d23c8435960f
Emil Lundberg <emil@yubico.com>
2019-01-03 18:10:02 +0100

    Expand JavadocGenerally test to cover current getter/setter copying behaviour

--- test/transform/resource/after-ecj/JavadocGenerally.java
+++ test/transform/resource/after-ecj/JavadocGenerally.java
@@ -3,9 +3,31 @@ class JavadocGenerally {
   public interface TestingInner {
   }
   private int someField;
+  private @lombok.Getter @lombok.Setter int someBeanField;
+  private @lombok.Getter int someGetterField;
+  private @lombok.Setter int someSetterField;
+  private @lombok.Getter @lombok.Setter int someGetterSetterField;
   JavadocGenerally() {
     super();
   }
   public void test() {
+  }
+  public @java.lang.SuppressWarnings("all") int getSomeBeanField() {
+    return this.someBeanField;
+  }
+  public @java.lang.SuppressWarnings("all") void setSomeBeanField(final int someBeanField) {
+    this.someBeanField = someBeanField;
+  }
+  public @java.lang.SuppressWarnings("all") int getSomeGetterField() {
+    return this.someGetterField;
+  }
+  public @java.lang.SuppressWarnings("all") void setSomeSetterField(final int someSetterField) {
+    this.someSetterField = someSetterField;
+  }
+  public @java.lang.SuppressWarnings("all") int getSomeGetterSetterField() {
+    return this.someGetterSetterField;
+  }
+  public @java.lang.SuppressWarnings("all") void setSomeGetterSetterField(final int someGetterSetterField) {
+    this.someGetterSetterField = someGetterSetterField;
   }
 }
--- test/transform/resource/before/JavadocGenerally.java
+++ test/transform/resource/before/JavadocGenerally.java
@@ -18,6 +18,63 @@ class JavadocGenerally {
 	private int someField;
 
 	/**
+	 * Doc on field
+	 * <pre>
+	 * 	// code
+	 * </pre>
+	 * @param someBeanField the new value for the field
+	 * @return the value of the field
+	 */
+	@lombok.Getter
+	@lombok.Setter
+	private int someBeanField;
+
+	/**
+	 * This will remain on the field
+	 * @param foo is not removed when there's a GETTER section
+	 * @return is not removed when there's a GETTER section
+	 *
+	 * --- GETTER ---
+	 * This will be moved to the getter
+	 * @param foo is not removed from a GETTER section
+	 * @return is not removed from a GETTER section
+	 */
+	@lombok.Getter
+	private int someGetterField;
+
+	/**
+	 * This will remain on the field
+	 * @param foo is not removed when there's a SETTER section
+	 * @return is not removed when there's a SETTER section
+	 *
+	 * --- SETTER ---
+	 * This will be moved to the setter
+	 * @param foo is not removed from a SETTER section
+	 * @return is not removed from a SETTER section
+	 */
+	@lombok.Setter
+	private int someSetterField;
+
+	/**
+	 * This will remain on the field
+	 * @param foo is not removed
+	 * @return is not removed
+	 *
+	 * --- GETTER ---
+	 * This will be moved to the getter
+	 * @param foo is not removed from a GETTER section
+	 * @return is not removed from a GETTER section
+	 *
+	 * --- SETTER ---
+	 * This will be moved to the setter
+	 * @param foo is not removed from a SETTER section
+	 * @return is not removed from a SETTER section
+	 */
+	@lombok.Getter
+	@lombok.Setter
+	private int someGetterSetterField;
+
+	/**
 	 * Doc on method
 	 */
 	public void test() {
