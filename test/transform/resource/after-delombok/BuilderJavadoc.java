f87359d3c9c52d08cfb0d3d2c9f8abc2611de2df
Emil Lundberg <emil@yubico.com>
2019-01-04 21:59:49 +0100

    Update BuilderJavadoc test to agree with current behaviour

--- test/transform/resource/after-delombok/BuilderJavadoc.java
+++ test/transform/resource/after-delombok/BuilderJavadoc.java
@@ -7,8 +7,6 @@ class BuilderJavadoc<T> {
 	/**
 	 * Yes, yes gets a setter.
 	 * @see #also
-	 *
-	 * @return tag is removed from the setter.
 	 */
 	private final int yes;
 	/**
@@ -17,14 +15,10 @@ class BuilderJavadoc<T> {
 	private int getsetwith;
 	/**
 	 * Predef has a predefined builder setter with no javadoc, and the builder setter does not get this one.
-	 * @param tag remains on the field.
-	 * @return tag remains on the field.
 	 */
 	private final int predef;
 	/**
 	 * predefWithJavadoc has a predefined builder setter with javadoc, so it keeps that one untouched.
-	 * @param tag is removed from the field.
-	 * @return tag remains on the field.
 	 */
 	private final int predefWithJavadoc;
 	private List<T> also;
