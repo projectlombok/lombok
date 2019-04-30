bf0499263b05e11fdd43886df3dc5663c8fee5f4
Reinier Zwitserloot <r.zwitserloot@projectlombok.org>
2019-05-01 01:33:57 +0200

    [issue #2092] Add an uppercase option to FieldNameConstants

---
+++ test/transform/resource/after-ecj/FieldNameConstantsUppercased.java
@@ -0,0 +1,23 @@
+import lombok.experimental.FieldNameConstants;
+import lombok.AccessLevel;
+public @FieldNameConstants(level = AccessLevel.PACKAGE) class FieldNameConstantsUppercased {
+  static final @java.lang.SuppressWarnings("all") class Fields {
+    public static final java.lang.String I_AM_A_DVD_PLAYER = "iAmADvdPlayer";
+    public static final java.lang.String BUT_PRINT_ME_PLEASE = "butPrintMePlease";
+    <clinit>() {
+    }
+    private @java.lang.SuppressWarnings("all") Fields() {
+      super();
+    }
+  }
+  String iAmADvdPlayer;
+  int $skipMe;
+  static double skipMeToo;
+  @FieldNameConstants.Exclude int andMe;
+  String butPrintMePlease;
+  <clinit>() {
+  }
+  public FieldNameConstantsUppercased() {
+    super();
+  }
+}
