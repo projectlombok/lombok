cd49188aff9ffece51ce8831c961e0faab33fbc8
Bulgakov Alexander <buls@yandex.ru>
2019-05-20 13:44:29 +0300

    #1976. one more test case

--- test/transform/resource/after-delombok/ValLambda.java
+++ test/transform/resource/after-delombok/ValLambda.java
@@ -13,6 +13,7 @@ class ValLambda {
 	public void easyLubLambda() {
 		final java.lang.Runnable foo = (System.currentTimeMillis() > 0) ? (Runnable) () -> {
 		} : System.out::println;
+		final java.lang.Runnable foo1 = (System.currentTimeMillis() > 0) ? (Runnable) System.out::println : System.out::println;
 		final java.util.function.Function foo2 = (System.currentTimeMillis() < 0) ? (java.util.function.Function) r -> "" : r -> System.currentTimeMillis();
 		java.util.function.Function foo3 = (System.currentTimeMillis() < 0) ? (java.util.function.Function) r -> "" : r -> System.currentTimeMillis();
 		final java.util.function.Function<java.lang.String, java.lang.String> foo4 = (System.currentTimeMillis() < 0) ? (java.util.function.Function<String, String>) r -> "" : r -> String.valueOf(System.currentTimeMillis());
