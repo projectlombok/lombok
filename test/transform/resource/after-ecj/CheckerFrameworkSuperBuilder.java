461b5541da3fc8c43439f032e2f1d37574a1a658
Reinier Zwitserloot <r.zwitserloot@projectlombok.org>
2019-08-26 22:22:05 +0200

    [tests] fixing an error in a superbuilder/checkerframework test

--- test/transform/resource/after-ecj/CheckerFrameworkSuperBuilder.java
+++ test/transform/resource/after-ecj/CheckerFrameworkSuperBuilder.java
@@ -96,7 +96,7 @@ class CheckerFrameworkSuperBuilder {
       public ChildBuilder() {
       }
       protected abstract @java.lang.Override @org.checkerframework.checker.builder.qual.ReturnsReceiver @org.checkerframework.dataflow.qual.SideEffectFree @java.lang.SuppressWarnings("all") B self();
-      protected abstract @java.lang.Override @org.checkerframework.dataflow.qual.SideEffectFree @java.lang.SuppressWarnings("all") C build(final @org.checkerframework.checker.builder.qual.CalledMethods("b") ChildBuilder this);
+      public abstract @java.lang.Override @org.checkerframework.dataflow.qual.SideEffectFree @java.lang.SuppressWarnings("all") C build(final @org.checkerframework.checker.builder.qual.CalledMethods("b") ChildBuilder this);
       public @org.checkerframework.checker.builder.qual.ReturnsReceiver @java.lang.SuppressWarnings("all") B a(final @org.checkerframework.checker.builder.qual.NotCalledMethods("a") ChildBuilder this, final int a) {
         this.a$value = a;
         a$set = true;
