7d08af7d856b41580fa4b913e2b0c9002a8fc341
Rawi01 <Rawi01@users.noreply.github.com>
2020-05-09 13:19:17 +0200

    [fixes #2382] Handle generic supertypes

---
+++ test/transform/resource/after-ecj/DelegateGenerics.java
@@ -0,0 +1,24 @@
+public class DelegateGenerics<T> {
+  @lombok.experimental.Delegate I1<T> target;
+  public DelegateGenerics() {
+    super();
+  }
+  public @java.lang.SuppressWarnings("all") T a(final T a) {
+    return this.target.a(a);
+  }
+  public @java.lang.SuppressWarnings("all") java.lang.String i(final java.lang.String a) {
+    return this.target.i(a);
+  }
+  public @java.lang.SuppressWarnings("all") java.lang.Integer t(final java.lang.Integer t) {
+    return this.target.t(t);
+  }
+}
+interface I1<T> extends I2<T, Integer, String> {
+}
+interface I2<A, T, I> extends I3<Integer, I, A> {
+}
+interface I3<T, I, A> {
+  public T t(T t);
+  public I i(I a);
+  public A a(A a);
+}
