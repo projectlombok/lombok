c7040d3acd12d63706f8b60a19261ce9a31abe75
murphy-li <13083353+murphy-li@users.noreply.github.com>
2020-12-15 17:13:35 +0800

    fix #2679

--- src/utils/lombok/permit/Permit.java
+++ src/utils/lombok/permit/Permit.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018-20199 The Project Lombok Authors.
+ * Copyright (C) 2018-2019 The Project Lombok Authors.
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
