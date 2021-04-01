0fb3a680ac0334eb231b47ba1b4bac94a8a34ed1
Reinier Zwitserloot <r.zwitserloot@projectlombok.org>
2021-04-02 00:47:30 +0200

    [build] fix the website maker's all-versions tool

--- src/support/lombok/website/WebsiteMaker.java
+++ src/support/lombok/website/WebsiteMaker.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2021 The Project Lombok Authors.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package lombok.website;
 
 import java.io.BufferedReader;
@@ -72,7 +93,7 @@ public class WebsiteMaker {
 	private static void printAllVersions(Domain domain) throws Exception {
 		List<List<String>> versions = readAllVersions(domain);
 		for (List<String> v : versions) {
-			System.out.println(" <a href=\"" + v.get(0) + "\">" + v.get(1) + "</a>");
+			System.out.println(" <a href=\"" + v.get(1) + "\">" + v.get(0) + "</a>");
 		}
 	}
 	
