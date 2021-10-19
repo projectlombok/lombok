890028646a93551c704f316e43348c13904193cd
ysherwin <ysherwin@amazon.com>
2021-10-19 14:01:28 +0300

    fixing beanspec tests

--- test/transform/resource/after-delombok/ValueWithJavaBeansSpecCapitalization.java
+++ test/transform/resource/after-delombok/ValueWithJavaBeansSpecCapitalization.java
@@ -31,36 +31,3 @@ final class ValueWithJavaBeansSpecCapitalization {
 		return "ValueWithJavaBeansSpecCapitalization(aField=" + this.getaField() + ")";
 	}
 }
-final class ValueWithoutJavaBeansSpecCapitalization {
-	private final int aField;
-	@java.lang.SuppressWarnings("all")
-	public ValueWithoutJavaBeansSpecCapitalization(final int aField) {
-		this.aField = aField;
-	}
-	@java.lang.SuppressWarnings("all")
-	public int getAField() {
-		return this.aField;
-	}
-	@java.lang.Override
-	@java.lang.SuppressWarnings("all")
-	public boolean equals(final java.lang.Object o) {
-		if (o == this) return true;
-		if (!(o instanceof ValueWithoutJavaBeansSpecCapitalization)) return false;
-		final ValueWithoutJavaBeansSpecCapitalization other = (ValueWithoutJavaBeansSpecCapitalization) o;
-		if (this.getAField() != other.getAField()) return false;
-		return true;
-	}
-	@java.lang.Override
-	@java.lang.SuppressWarnings("all")
-	public int hashCode() {
-		final int PRIME = 59;
-		int result = 1;
-		result = result * PRIME + this.getAField();
-		return result;
-	}
-	@java.lang.Override
-	@java.lang.SuppressWarnings("all")
-	public java.lang.String toString() {
-		return "ValueWithoutJavaBeansSpecCapitalization(aField=" + this.getAField() + ")";
-	}
-}
