479bd2d75d2046ee1cb337ef6aa929b707868345
Rawi01 <Rawi01@users.noreply.github.com>
2022-08-26 00:53:47 +0200

    [jdk19] Add support for record pattern

---
+++ test/pretty/resource/before/RecordPattern.java
@@ -0,0 +1,16 @@
+// version 19:
+record Point(int x, int y) {
+}
+record Rectangle(Point upperLeft, Point lowerRight) {
+}
+
+public class RecordPattern {
+	void recordPattern(Object o) {
+		if (o instanceof Point(int x, int y)) {
+		}
+		if (o instanceof Point(int x, int y) p) {
+		}
+		if (o instanceof Rectangle(Point(int x1, int y1), Point(int x2, int y2))) {
+		}
+	}
+}
