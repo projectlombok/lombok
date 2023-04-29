8aa54b7f25537e84d812adf39a563fbe0de9871d
Rawi01 <Rawi01@users.noreply.github.com>
2023-04-29 09:24:11 +0200

    [fixes #3416] Ignore annotations during array detection

---
+++ test/transform/resource/after-ecj/ToStringArray.java
@@ -0,0 +1,12 @@
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+public @lombok.ToString class ToStringArray {
+  int[] primitiveArray;
+  Object[] objectArray;
+  public ToStringArray() {
+    super();
+  }
+  public @java.lang.Override @java.lang.SuppressWarnings("all") java.lang.String toString() {
+    return (((("ToStringArray(primitiveArray=" + java.util.Arrays.toString(this.primitiveArray)) + ", objectArray=") + java.util.Arrays.deepToString(this.objectArray)) + ")");
+  }
+}
---
+++ test/transform/resource/after-ecj/ToStringArrayTypeAnnotations.java
@@ -0,0 +1,16 @@
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+public @lombok.ToString class ToStringArrayTypeAnnotations {
+  @Target({ElementType.TYPE_USE}) @interface TA {
+  }
+  @TA int[] primitiveArray1;
+  int @TA [] primitiveArray2;
+  @TA Object[] objectArray1;
+  Object @TA [] objectArray2;
+  public ToStringArrayTypeAnnotations() {
+    super();
+  }
+  public @java.lang.Override @java.lang.SuppressWarnings("all") java.lang.String toString() {
+    return (((((((("ToStringArrayTypeAnnotations(primitiveArray1=" + java.util.Arrays.toString(this.primitiveArray1)) + ", primitiveArray2=") + java.util.Arrays.toString(this.primitiveArray2)) + ", objectArray1=") + java.util.Arrays.deepToString(this.objectArray1)) + ", objectArray2=") + java.util.Arrays.deepToString(this.objectArray2)) + ")");
+  }
+}
