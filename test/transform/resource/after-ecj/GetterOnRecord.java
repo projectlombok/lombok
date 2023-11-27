a3dcd2e2c14e6ca8fd068861fb57734920cf380b
mjaggard <mat@billforward.net>
2023-11-27 08:22:27 +0000

    Update GetterOnRecord.java

--- test/transform/resource/after-ecj/GetterOnRecord.java
+++ test/transform/resource/after-ecj/GetterOnRecord.java
@@ -3,11 +3,6 @@ import lombok.Getter;
 public @Getter record GetterOnRecord(String a, String b) {
 /* Implicit */  private final String a;
 /* Implicit */  private final String b;
-  public GetterOnRecord(String a, String b) {
-    super();
-    .a = a;
-    .b = b;
-  }
 
   public @java.lang.SuppressWarnings("all") String getA() {
     return this.a;
