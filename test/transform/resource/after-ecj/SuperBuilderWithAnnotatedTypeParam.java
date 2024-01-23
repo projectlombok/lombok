603d512f93dab073b164661a408ecc6ec5313a68
Jan Rieke <rieke@subshell.com>
2024-01-23 13:57:38 +0100

    only run test for java >= 8

--- test/transform/resource/after-ecj/SuperBuilderWithAnnotatedTypeParam.java
+++ test/transform/resource/after-ecj/SuperBuilderWithAnnotatedTypeParam.java
@@ -1,3 +1,4 @@
+//version 8:
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
--- test/transform/resource/before/SuperBuilderWithAnnotatedTypeParam.java
+++ test/transform/resource/before/SuperBuilderWithAnnotatedTypeParam.java
@@ -1,4 +1,4 @@
-//VERSION 8:
+//version 8:
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
