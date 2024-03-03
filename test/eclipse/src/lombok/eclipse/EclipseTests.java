4177fd50df2eb54fefd028f1932d01985f65fa10
Rawi01 <Rawi01@users.noreply.github.com>
2024-03-03 12:18:53 +0100

    Fix Javadoc in Eclipse

--- test/eclipse/src/lombok/eclipse/EclipseTests.java
+++ test/eclipse/src/lombok/eclipse/EclipseTests.java
@@ -28,13 +28,14 @@ import org.junit.runners.Suite.SuiteClasses;
 import lombok.eclipse.cleanup.CleanupTest;
 import lombok.eclipse.compile.NoErrorsTest;
 import lombok.eclipse.edit.SelectTest;
+import lombok.eclipse.misc.JavadocTest;
 import lombok.eclipse.refactoring.ExtractInterfaceTest;
 import lombok.eclipse.refactoring.InlineTest;
 import lombok.eclipse.refactoring.RenameTest;
 import lombok.eclipse.references.FindReferencesTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ExtractInterfaceTest.class, RenameTest.class, SelectTest.class, CleanupTest.class, FindReferencesTest.class, InlineTest.class, NoErrorsTest.class})
+@SuiteClasses({ExtractInterfaceTest.class, RenameTest.class, SelectTest.class, CleanupTest.class, FindReferencesTest.class, InlineTest.class, NoErrorsTest.class, JavadocTest.class})
 public class EclipseTests {
 	
 }
