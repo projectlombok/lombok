b4ee8aea7ba1ff71506b836f95fd68ec527764c3
Jan Rieke <rieke@subshell.com>
2024-11-01 23:09:48 +0100

    document rationale behind Jackson annotation copying

--- src/core/lombok/eclipse/handlers/EclipseHandlerUtil.java
+++ src/core/lombok/eclipse/handlers/EclipseHandlerUtil.java
@@ -823,21 +823,21 @@ public class EclipseHandlerUtil {
 	 * Searches the given field node for annotations that are specifically intentioned to be copied to the setter.
 	 */
 	public static Annotation[] findCopyableToGetterAnnotations(EclipseNode node) {
-		return findAnnotationsInList(node, COPY_TO_GETTER_ANNOTATIONS);
+		return findAnnotationsInList(node, JACKSON_COPY_TO_GETTER_ANNOTATIONS);
 	}
 
 	/**
 	 * Searches the given field node for annotations that are specifically intentioned to be copied to the setter.
 	 */
 	public static Annotation[] findCopyableToSetterAnnotations(EclipseNode node) {
-		return findAnnotationsInList(node, COPY_TO_SETTER_ANNOTATIONS);
+		return findAnnotationsInList(node, JACKSON_COPY_TO_SETTER_ANNOTATIONS);
 	}
 
 	/**
 	 * Searches the given field node for annotations that are specifically intentioned to be copied to the builder's singular method.
 	 */
 	public static Annotation[] findCopyableToBuilderSingularSetterAnnotations(EclipseNode node) {
-		return findAnnotationsInList(node, COPY_TO_BUILDER_SINGULAR_SETTER_ANNOTATIONS);
+		return findAnnotationsInList(node, JACKSON_COPY_TO_BUILDER_SINGULAR_SETTER_ANNOTATIONS);
 	}
 	
 	/**
