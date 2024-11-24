4294ebd301ae18d196474d65083b9c5f9b48b70e
Rawi01 <Rawi01@users.noreply.github.com>
2024-11-24 20:58:41 +0100

    [fixes #3605] Remove outer type reference when marking class as static
    The class now implements Serializable to trigger generation of this$0
    fields in the inner class. Since Java 18 the compiler only generates the
    reference to the outer class if it is in use or the class is
    Serializable.

--- test/transform/resource/after-ecj/UtilityClassInner.java
+++ test/transform/resource/after-ecj/UtilityClassInner.java
@@ -1,5 +1,5 @@
-class UtilityClassInner {
-  static final @lombok.experimental.UtilityClass class UtilClass {
+@java.lang.SuppressWarnings("serial") class UtilityClassInner {
+  static final @lombok.experimental.UtilityClass class UtilClass implements java.io.Serializable {
     private @java.lang.SuppressWarnings("all") @lombok.Generated UtilClass() {
       super();
       throw new java.lang.UnsupportedOperationException("This is a utility class and cannot be instantiated");
