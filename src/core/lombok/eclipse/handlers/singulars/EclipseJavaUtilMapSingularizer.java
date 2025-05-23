ddbdaba889faf3e7450ed7e6303dd8b9fc1989ad
Reinier Zwitserloot <r.zwitserloot@projectlombok.org>
2025-05-23 02:50:09 +0200

    [trivial] Update copyright years

--- src/core/lombok/eclipse/handlers/singulars/EclipseJavaUtilMapSingularizer.java
+++ src/core/lombok/eclipse/handlers/singulars/EclipseJavaUtilMapSingularizer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015-2021 The Project Lombok Authors.
+ * Copyright (C) 2015-2025 The Project Lombok Authors.
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
