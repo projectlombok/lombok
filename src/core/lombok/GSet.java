b4165990e40c0d0ad478a926eee3db3543399e16
JNGDSOFT <jngd.public@gmail.com>
2025-05-25 20:59:44 -0500

    Annotation and its handler were created

---
+++ src/core/lombok/GSet.java
@@ -0,0 +1,12 @@
+package lombok;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD, ElementType.TYPE})
+@Retention(RetentionPolicy.SOURCE)
+public @interface GSet {
+    
+}
---
+++ src/core/lombok/javac/handlers/HandleGSet.java
@@ -0,0 +1,5 @@
+package lombok.javac.handlers;
+
+public class HandleGSet {
+    
+}
