87e8af476a40b9a7b1c53a82a0fd1d8ba1ead707
Reinier Zwitserloot <r.zwitserloot@projectlombok.org>
2025-08-07 02:02:02 +0200

    [comment] Added explanation: We skip ecj check on `NonNullOnRecordExistingConstructorz`.

--- test/transform/resource/after-ecj/NonNullOnRecordExistingConstructor.java
+++ test/transform/resource/after-ecj/NonNullOnRecordExistingConstructor.java
@@ -1,5 +1,5 @@
 // version 14:
-// skip compare content
+// skip compare content: At time of writing, the `@NonNull` annotation causes our handler to produce an explicit-params constructor (i.e. `public NNOREC(String a) {` instead of `public NNOREC {`) with `@NonNull`, and in the statements the null check. But (?) other eclipse versions might not, hence, we ignore content.
 import lombok.NonNull;
 public record NonNullOnRecordExistingConstructor(String a) {
   public NonNullOnRecordExistingConstructor {
