7bf709420fc4ec083a4309de9871038795083baa
Liam Pace <146035497+liampace@users.noreply.github.com>
2025-08-18 18:19:52 -0400

    Add before transforms for various loggers

---
+++ test/transform/resource/before/LoggerCustomAccess.java
@@ -0,0 +1,26 @@
+import lombok.AccessLevel;
+import lombok.CustomLog;
+
+@CustomLog(access=AccessLevel.PUBLIC)
+class LoggerCustomAccessPublic {
+}
+
+@CustomLog(access=AccessLevel.MODULE)
+class LoggerCustomAccessModule {
+}
+
+@CustomLog(access=AccessLevel.PROTECTED)
+class LoggerCustomAccessProtected {
+}
+
+@CustomLog(access=AccessLevel.PACKAGE)
+class LoggerCustomAccessPackage {
+}
+
+@CustomLog(access=AccessLevel.PRIVATE)
+class LoggerCustomAccessPrivate {
+}
+
+@CustomLog(access=AccessLevel.NONE)
+class LoggerCustomAccessNone {
+}
