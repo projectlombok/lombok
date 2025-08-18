9a2e7cb4091e13807a24c1c5329ac91e4ad95351
Liam Pace <146035497+liampace@users.noreply.github.com>
2025-08-18 19:04:26 -0400

    Add after-delombok transforms for various loggers

---
+++ test/transform/resource/after-delombok/LoggerCommonsAccess.java
@@ -0,0 +1,33 @@
+import lombok.AccessLevel;
+class LoggerCommonsAccessPublic {
+	@java.lang.SuppressWarnings("all")
+	@lombok.Generated
+	public static final org.apache.commons.logging.Log log = org.apache.commons.logging.LogFactory.getLog(LoggerCommonsAccessPublic.class);
+
+}
+class LoggerCommonsAccessModule {
+	@java.lang.SuppressWarnings("all")
+	@lombok.Generated
+	static final org.apache.commons.logging.Log log = org.apache.commons.logging.LogFactory.getLog(LoggerCommonsAccessModule.class);
+
+}
+class LoggerCommonsAccessProtected {
+	@java.lang.SuppressWarnings("all")
+	@lombok.Generated
+	protected static final org.apache.commons.logging.Log log = org.apache.commons.logging.LogFactory.getLog(LoggerCommonsAccessProtected.class);
+
+}
+class LoggerCommonsAccessPackage {
+	@java.lang.SuppressWarnings("all")
+	@lombok.Generated
+	static final org.apache.commons.logging.Log log = org.apache.commons.logging.LogFactory.getLog(LoggerCommonsAccessPackage.class);
+
+}
+class LoggerCommonsAccessPrivate {
+	@java.lang.SuppressWarnings("all")
+	@lombok.Generated
+	private static final org.apache.commons.logging.Log log = org.apache.commons.logging.LogFactory.getLog(LoggerCommonsAccessPrivate.class);
+
+}
+class LoggerCommonsAccessNone {
+}
---
+++ test/transform/resource/after-delombok/LoggerFloggerAccess.java
@@ -0,0 +1,33 @@
+import lombok.AccessLevel;
+class LoggerFloggerAccessPublic {
+	@java.lang.SuppressWarnings("all")
+	@lombok.Generated
+	public static final com.google.common.flogger.FluentLogger log = com.google.common.flogger.FluentLogger.forEnclosingClass();
+
+}
+class LoggerFloggerAccessModule {
+	@java.lang.SuppressWarnings("all")
+	@lombok.Generated
+	static final com.google.common.flogger.FluentLogger log = com.google.common.flogger.FluentLogger.forEnclosingClass();
+
+}
+class LoggerFloggerAccessProtected {
+	@java.lang.SuppressWarnings("all")
+	@lombok.Generated
+	protected static final com.google.common.flogger.FluentLogger log = com.google.common.flogger.FluentLogger.forEnclosingClass();
+
+}
+class LoggerFloggerAccessPackage {
+	@java.lang.SuppressWarnings("all")
+	@lombok.Generated
+	static final com.google.common.flogger.FluentLogger log = com.google.common.flogger.FluentLogger.forEnclosingClass();
+
+}
+class LoggerFloggerAccessPrivate {
+	@java.lang.SuppressWarnings("all")
+	@lombok.Generated
+	private static final com.google.common.flogger.FluentLogger log = com.google.common.flogger.FluentLogger.forEnclosingClass();
+
+}
+class LoggerFloggerAccessNone {
+}
