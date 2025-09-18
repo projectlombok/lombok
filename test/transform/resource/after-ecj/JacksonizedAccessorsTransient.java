4a99b3b57ac1c20851338e244bb0f3e8c685737d
Liam Pace <146035497+liampace@users.noreply.github.com>
2025-09-18 12:06:25 -0400

    Remove redundant @JsonIgnore annotation

--- test/transform/resource/after-ecj/JacksonizedAccessorsTransient.java
+++ test/transform/resource/after-ecj/JacksonizedAccessorsTransient.java
@@ -1,15 +1,15 @@
 public @lombok.extern.jackson.Jacksonized @lombok.experimental.Accessors(fluent = true) @lombok.Getter @lombok.Setter class JacksonizedAccessorsTransient {
-  private transient @com.fasterxml.jackson.annotation.JsonIgnore int intValue;
+  private transient int intValue;
   public JacksonizedAccessorsTransient() {
     super();
   }
-  public @com.fasterxml.jackson.annotation.JsonIgnore @java.lang.SuppressWarnings("all") @lombok.Generated int intValue() {
+  public @java.lang.SuppressWarnings("all") @lombok.Generated int intValue() {
     return this.intValue;
   }
   /**
    * @return {@code this}.
    */
-  public @com.fasterxml.jackson.annotation.JsonIgnore @java.lang.SuppressWarnings("all") @lombok.Generated JacksonizedAccessorsTransient intValue(final int intValue) {
+  public @java.lang.SuppressWarnings("all") @lombok.Generated JacksonizedAccessorsTransient intValue(final int intValue) {
     this.intValue = intValue;
     return this;
   }
