da5e18de59370668a3b4aed960ae407308ca338e
Liam Pace <146035497+liampace@users.noreply.github.com>
2025-09-19 06:00:51 -0400

    rename LoggerXslf4jAccess* classes to match file name

--- test/transform/resource/after-ecj/LoggerXSlf4jAccess.java
+++ test/transform/resource/after-ecj/LoggerXSlf4jAccess.java
@@ -1,52 +1,52 @@
 import lombok.AccessLevel;
 import lombok.extern.slf4j.XSlf4j;
-@XSlf4j(access = AccessLevel.PUBLIC) class LoggerXslf4jAccessPublic {
-  public static final @java.lang.SuppressWarnings("all") @lombok.Generated org.slf4j.ext.XLogger log = org.slf4j.ext.XLoggerFactory.getXLogger(LoggerXslf4jAccessPublic.class);
+@XSlf4j(access = AccessLevel.PUBLIC) class LoggerXSlf4jAccessPublic {
+  public static final @java.lang.SuppressWarnings("all") @lombok.Generated org.slf4j.ext.XLogger log = org.slf4j.ext.XLoggerFactory.getXLogger(LoggerXSlf4jAccessPublic.class);
   <clinit>() {
   }
 
-  LoggerXslf4jAccessPublic() {
+  LoggerXSlf4jAccessPublic() {
     super();
   }
 }
-@SuppressWarnings("deprecation") @XSlf4j(access = AccessLevel.MODULE) class LoggerXslf4jAccessModule {
-  static final @java.lang.SuppressWarnings("all") @lombok.Generated org.slf4j.ext.XLogger log = org.slf4j.ext.XLoggerFactory.getXLogger(LoggerXslf4jAccessModule.class);
+@SuppressWarnings("deprecation") @XSlf4j(access = AccessLevel.MODULE) class LoggerXSlf4jAccessModule {
+  static final @java.lang.SuppressWarnings("all") @lombok.Generated org.slf4j.ext.XLogger log = org.slf4j.ext.XLoggerFactory.getXLogger(LoggerXSlf4jAccessModule.class);
   <clinit>() {
   }
 
-  LoggerXslf4jAccessModule() {
+  LoggerXSlf4jAccessModule() {
     super();
   }
 }
-@XSlf4j(access = AccessLevel.PROTECTED) class LoggerXslf4jAccessProtected {
-  protected static final @java.lang.SuppressWarnings("all") @lombok.Generated org.slf4j.ext.XLogger log = org.slf4j.ext.XLoggerFactory.getXLogger(LoggerXslf4jAccessProtected.class);
+@XSlf4j(access = AccessLevel.PROTECTED) class LoggerXSlf4jAccessProtected {
+  protected static final @java.lang.SuppressWarnings("all") @lombok.Generated org.slf4j.ext.XLogger log = org.slf4j.ext.XLoggerFactory.getXLogger(LoggerXSlf4jAccessProtected.class);
   <clinit>() {
   }
 
-  LoggerXslf4jAccessProtected() {
+  LoggerXSlf4jAccessProtected() {
     super();
   }
 }
-@XSlf4j(access = AccessLevel.PACKAGE) class LoggerXslf4jAccessPackage {
-  static final @java.lang.SuppressWarnings("all") @lombok.Generated org.slf4j.ext.XLogger log = org.slf4j.ext.XLoggerFactory.getXLogger(LoggerXslf4jAccessPackage.class);
+@XSlf4j(access = AccessLevel.PACKAGE) class LoggerXSlf4jAccessPackage {
+  static final @java.lang.SuppressWarnings("all") @lombok.Generated org.slf4j.ext.XLogger log = org.slf4j.ext.XLoggerFactory.getXLogger(LoggerXSlf4jAccessPackage.class);
   <clinit>() {
   }
 
-  LoggerXslf4jAccessPackage() {
+  LoggerXSlf4jAccessPackage() {
     super();
   }
 }
-@XSlf4j(access = AccessLevel.PRIVATE) class LoggerXslf4jAccessPrivate {
-  private static final @java.lang.SuppressWarnings("all") @lombok.Generated org.slf4j.ext.XLogger log = org.slf4j.ext.XLoggerFactory.getXLogger(LoggerXslf4jAccessPrivate.class);
+@XSlf4j(access = AccessLevel.PRIVATE) class LoggerXSlf4jAccessPrivate {
+  private static final @java.lang.SuppressWarnings("all") @lombok.Generated org.slf4j.ext.XLogger log = org.slf4j.ext.XLoggerFactory.getXLogger(LoggerXSlf4jAccessPrivate.class);
   <clinit>() {
   }
 
-  LoggerXslf4jAccessPrivate() {
+  LoggerXSlf4jAccessPrivate() {
     super();
   }
 }
-@XSlf4j(access = AccessLevel.NONE) class LoggerXslf4jAccessNone {
-  LoggerXslf4jAccessNone() {
+@XSlf4j(access = AccessLevel.NONE) class LoggerXSlf4jAccessNone {
+  LoggerXSlf4jAccessNone() {
     super();
   }
 }
