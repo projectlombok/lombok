f1c57b52a7eb0a097c58b920cbe37df6b496c08d
Jan Rieke <rieke@subshell.com>
2025-09-30 00:27:46 +0200

    ecj: fix duplicate jackson annotation copying when generating setter

---
+++ test/transform/resource/before/JacksonJsonProperty2.java
@@ -0,0 +1,12 @@
+//CONF: lombok.copyJacksonAnnotationsToAccessors = true
+//version 8: Jackson deps are at least Java7+.
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data public class JacksonJsonProperty3 {
+	@JsonProperty("a") String propertyOne;
+	@JsonProperty("b") int propertyTwo;
+	Map<String, Object> additional = new HashMap<>();
+}
