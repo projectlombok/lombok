3d26cdc2139710ec688227221b8cb06a09ab4787
Jan Rieke <rieke@subshell.com>
2025-09-30 13:21:54 +0200

    fix test

--- test/transform/resource/before/JacksonJsonProperty2.java
+++ test/transform/resource/before/JacksonJsonProperty2.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
-@Data public class JacksonJsonProperty3 {
+@Data public class JacksonJsonProperty2 {
 	@JsonProperty("a") String propertyOne;
 	@JsonProperty("b") int propertyTwo;
 	Map<String, Object> additional = new HashMap<>();
