f227a87c1f43ca720df4e79456bed35ba6fce10e
Dymitr <43708417+ay0ks@users.noreply.github.com>
2025-12-03 14:01:35 +0400

    Expand SuperBuilderCustomized.java to cover constructorExists fix
    If I'm not mistaken, `builder()` should accept the generics at line 106.
    I haven't touched this for a really long time, so excuse me.

--- test/transform/resource/before/SuperBuilderCustomized.java
+++ test/transform/resource/before/SuperBuilderCustomized.java
@@ -41,8 +41,68 @@ public class SuperBuilderCustomized {
 			return new ChildBuilderImpl().field2(10.0);
 		}
 	}
+
+	@lombok.experimental.SuperBuilder
+	public static abstract class AbstractParent2<T extends Integer> {
+	    @lombok.Getter
+	    protected final T field1;
+	
+	    @lombok.Getter
+	    @lombok.Builder.Default
+	    protected final T field2 = null;
+	
+	    public static abstract class AbstractParent2Builder<
+	            T extends Integer,
+	            C extends AbstractParent2<T>,
+	            B extends AbstractParent2Builder<T, C, B>> {
+	        private B field2(final T ignored) {
+	            return self();
+	        }
+	    }
+	
+	    public AbstractParent2(final AbstractParent2Builder<T, ?, ?> builder) {
+	        this.field1 = builder.field1$value;
+	        this.field2 = AbstractParent2.obtainField2();
+	    }
+
+		public static T obtainField2() {
+			return (T) 123;
+		}
+	}
+	
+	@lombok.experimental.SuperBuilder
+	public static class Child2<T extends Integer, TT extends Integer> extends AbstractParent2<T> {
+	    @lombok.Getter
+	    protected final T field3;
+	
+	    @lombok.Getter
+	    @lombok.Builder.Default
+	    protected final T field4 = null;
+	
+	    public static abstract class Child2Builder<
+	            T extends Integer,
+	            TT extends Integer,
+	            C extends Child2<T, TT>,
+	            B extends Child2Builder<T, TT, C, B>>
+	            extends AbstractParent2Builder<T, C, B> {
+	        private B field4(final TT ignored) {
+	            return self();
+	        }
+	    }
+	
+	    public Child(final Child2Builder<T, TT, ?, ?> builder) {
+	        super(builder);
+	        this.field3 = builder.field3$value;
+	        this.field4 = Child2.obtainField4();
+	    }
+
+		public static T obtainField4() {
+			return (T) 456;
+		}
+	}
 	
 	public static void test() {
 		Child x = Child.builder().field2(1.0).field1(5).resetToDefault().build();
+		Child2 y = Child2.<Integer, Integer>builder().field1(234).field3(567).build();
 	}
 }
